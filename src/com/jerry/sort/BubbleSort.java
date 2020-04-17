ddbda61a7c4a34be53538099e5b15fe24a037b45
雷俊俊 <junjun.lei@hand-china.com>
2020-04-17 14:58:46 +0800

    选择排序

--- src/com/jerry/sort/BubbleSort.java
+++ src/com/jerry/sort/BubbleSort.java
@@ -3,14 +3,25 @@ package com.jerry.sort;
 import java.util.Arrays;
 
 /**
+ * 冒泡排序
+ *
  * @author Jerry
  * @create 2020-04-09 23:01
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int []arr={1,-1,3,5,4,8,7};
+        //int []arr={1,-1,3,5,4,8,7};
+        int[] arr = new int[80000];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = (int) (Math.random() * 80000);
+        }
+        long start = System.currentTimeMillis();
         sort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
+
+    //时间复杂度     O(n²)
 
     public static void sort(int[] source) {
         int temp;
@@ -18,10 +29,10 @@ public class BubbleSort {
         for (int i = 1; i < source.length; i++) {
             //内层循环表每趟比较次数
             for (int j = 0; j < source.length - i; j++) {
-                if(source[j]>source[j+1]){
-                    temp=source[j+1];
-                    source[j+1]=source[j];
-                    source[j]=temp;
+                if (source[j] > source[j + 1]) {
+                    temp = source[j + 1];
+                    source[j + 1] = source[j];
+                    source[j] = temp;
                 }
             }
         }
---
+++ src/com/jerry/sort/SelectSort.java
@@ -0,0 +1,41 @@
+package com.jerry.sort;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ * @author Jerry
+ * @create 2020-04-10 12:18
+ */
+public class SelectSort {
+
+    public static void main(String[] args) {
+        //int []arr={1,-1,3,5,4,8,7};
+        int []arr=new int[80000];
+        for(int i=0;i<arr.length-1;i++){
+            arr[i]= (int) (Math.random()*80000);
+        }
+        long start = System.currentTimeMillis();
+        selectSort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = arr[i];
+            int minIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                   min=arr[j];
+                   minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
